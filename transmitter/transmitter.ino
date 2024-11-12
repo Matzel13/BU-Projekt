4fcbcb611afe0ad62a8af0776900d4e71b09fba9
haw-peter <167177670+haw-peter@users.noreply.github.com>
2024-11-12 15:21:58 +0100

    Update transmitter.ino

--- transmitter/transmitter.ino
+++ transmitter/transmitter.ino
@@ -1,15 +1,20 @@
+#define COMM 16
 
-#define COMM  16
+#define ROW1 2
+#define ROW2 3
+#define COL1 4
+#define COL2 5
 
-#define ROW1  2
-#define ROW2  3
-#define COL1  4
-#define COL2  5
+int ROWS[2] {
+  ROW1,
+  ROW2
+};
+int COLS[2] {
+  COL1,
+  COL2
+};
 
-int ROWS[2] {ROW1, ROW2};
-int COLS[2] {COL1, COL2};
-
-#define DELAY delayMicroseconds(1000)  // time between bits
+#define DELAY delayMicroseconds(1000) // time between bits
 //#define message 'P' // 0x50
 
 int adressSize = 3;
@@ -21,7 +26,7 @@ volatile int sizeOfMessage = 2;
 volatile char pressedButton = NULL;
 volatile unsigned input = 0x0000;
 
-void sendMessage(unsigned message, char adress){
+void sendMessage(unsigned message, char adress) {
   //char message[] = {'H', 'k', 'K', 0, 0, 0, 0};
   //adress = 0x05;
   Serial.print("Sending: ");
@@ -36,45 +41,42 @@ void sendMessage(unsigned message, char adress){
 
   // Adress:
   Serial.print("Adresse: ");
-  for(int i = 0; i < (adressSize+1); i++){
-    if((adress & 0x01) == 0x01){ 
+  for (int i = 0; i < (adressSize + 1); i++) {
+    if ((adress & 0x01) == 0x01) {
       digitalWrite(COMM, HIGH);
       Serial.print("1");
-    }
-    else{
+    } else {
       digitalWrite(COMM, LOW);
       Serial.print("0");
     }
     DELAY;
     adress = adress >> 1;
-  }  
+  }
   Serial.println();
 
   // COF (Bit count: 3 DataBits,)
   Serial.print("COF: ");
   char byteCount = ((char) sizeOfMessage);
-  for(int i = 0; i < 3; i++){
-    if((byteCount & 0x01) == 0x01){ 
+  for (int i = 0; i < 3; i++) {
+    if ((byteCount & 0x01) == 0x01) {
       digitalWrite(COMM, HIGH);
       Serial.print("1");
-    }
-    else{
+    } else {
       digitalWrite(COMM, LOW);
       Serial.print("0");
     }
     DELAY;
     byteCount = byteCount >> 1;
   }
-  Serial.println();  
+  Serial.println();
 
   // DATA:
-  for(int j = 0; j < sizeOfMessage; j++){
-    for(int i = 0; i < 8; i++){
-      if((message & 0x01) == 0x01){ 
+  for (int j = 0; j < sizeOfMessage; j++) {
+    for (int i = 0; i < 8; i++) {
+      if ((message & 0x01) == 0x01) {
         digitalWrite(COMM, HIGH);
         Serial.print("1");
-      }
-      else{
+      } else {
         digitalWrite(COMM, LOW);
         Serial.print("0");
       }
@@ -90,20 +92,20 @@ void sendMessage(unsigned message, char adress){
   Serial.print("1");
   DELAY;
   digitalWrite(COMM, LOW);
-  for(int i = 0; i < 5; i++){
-      Serial.print("0");
+  for (int i = 0; i < 5; i++) {
+    Serial.print("0");
     DELAY;
-  } 
+  }
   Serial.println();
 }
 
 // read the input keys:
-void keypad(){
+void keypad() {
   input = 0x0000;
-  for(int row = 0; row < 2; row++){
+  for (int row = 0; row < 2; row++) {
     digitalWrite(ROWS[row], HIGH);
-    for(int col = 0; col < 2; col++){
-      if(digitalRead(COLS[col]) == HIGH){
+    for (int col = 0; col < 2; col++) {
+      if (digitalRead(COLS[col]) == HIGH) {
         input = input ^ (0x0001 << col);
         Serial.println(col);
       }
@@ -132,16 +134,13 @@ void loop() {
 
   // constantly check for pressed buttons:
   keypad();
-  if(input != 0x0000){
-    if((input == 0x0020) ||(input == 0x0002)){
+  if (input != 0x0000) {
+    if ((input == 0x0020) || (input == 0x0002)) {
       sendMessage(input, 0x02);
     }
-    if((input == 0x0040) ||(input == 0x0004)){
+    if ((input == 0x0040) || (input == 0x0004)) {
       sendMessage(input, 0x01);
     }
   }
   delay(100); // delay 1 ms
 }
-
-
-
