0452cbe52e977410db7edd4db6f52f1be1fed370
Phoenix0704 <91696047+Phoenix0704@users.noreply.github.com>
2024-12-07 12:32:14 +0100

    überarbeitete Version der ESP Software

---
+++ Neuer Ordner/test/include/header.h
@@ -0,0 +1,40 @@
+#include <Arduino.h>
+#include <iostream>
+#include <list>
+//recieve
+void sync() ;
+
+char readAdress() ;
+
+char readCOF();
+
+void readData(char dataSize) ;
+
+char readMessage();
+
+//transmit
+
+void sendAdress(char adress) ;
+
+void sendCOF(char dataSize);
+
+void sendData(char data) ;
+
+void sendMessage();
+
+
+
+
+void setup() ;
+
+void printAdr(std::list<char>& Function, int stelle);
+
+int whichFunction(char adresse) ;
+
+int whichFunction(char adresse, volatile char* data);
+
+void functionKeypad(volatile char[]);
+
+void functionAudiopad(volatile char[]);
+
+void switchFunction(char adresse);
